c02f01bd976f95e1e4607535cf824354af053f00
MichelArenas <michel.arenas28572@ucaldas.edu.co>
2025-09-22 20:38:41 -0500

    CORS implementation

--- back-project/src/index.js
+++ back-project/src/index.js
@@ -2,11 +2,20 @@ const express = require("express");
 const database = require("./database/database");
 const bodyparser = require("body-parser");
 const routes = require("./router/routes");
+const cors = require("cors");
+
 require("dotenv").config();
 
 const port = process.env.PORT || 3000;
 
 const app = express();
+
+// Permitir CORS para tu frontend
+app.use(cors({
+  origin: "http://localhost:3000", // donde corre tu frontend
+  credentials: true,               // si usas cookies o auth headers
+}));
+
 app.use(bodyparser.json());
 app.use('/api/v1', routes);
 
